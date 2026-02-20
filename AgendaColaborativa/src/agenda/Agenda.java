1e3521dbc8b5e852ba62125184cdc1bc27f4d222
atore <atore@Antonio-1>
2026-02-20 13:20:33 +0100

    Actualización de la clase agenda, se le añadió un arrayList, constructor, getters y setters

--- AgendaColaborativa/src/agenda/Agenda.java
+++ AgendaColaborativa/src/agenda/Agenda.java
@@ -1,21 +1,49 @@
 package agenda;
 
-public class Agenda {
-	public Contacto buscarContacto(String nombre) {
-	    for (Contacto c : contactos) {
-	        if (c.getNombre().equalsIgnoreCase(nombre)) {
-	            return c;
-	        }
-	    }
-	    return null;
-	}
+import java.util.ArrayList;
+import java.util.List;
 
-	public boolean eliminarContacto(String nombre) {
-	    Contacto contacto = buscarContacto(nombre);
-	    if (contacto != null) {
-	        contactos.remove(contacto);
-	        return true;
-	    }
-	    return false;
-	}
+public class Agenda {
+
+    private List<Contacto> contactos;
+
+    public Agenda() {
+        contactos = new ArrayList<>();
+    }
+
+    public List<Contacto> getContactos() {
+        return contactos;
+    }
+
+    public void setContactos(List<Contacto> contactos) {
+        this.contactos = contactos;
+    }
+
+    public void agregarContacto(Contacto c) {
+        contactos.add(c);
+    }
+
+    public Contacto buscarContacto(String nombre) {
+        for (Contacto c : contactos) {
+            if (c.getNombre().equalsIgnoreCase(nombre)) {
+                return c;
+            }
+        }
+        return null;
+    }
+
+    public boolean eliminarContacto(String nombre) {
+        Contacto contacto = buscarContacto(nombre);
+        if (contacto != null) {
+            contactos.remove(contacto);
+            return true;
+        }
+        return false;
+    }
+
+    public void listarContactos() {
+        for (Contacto c : contactos) {
+            System.out.println(c);
+        }
+    }
 }
