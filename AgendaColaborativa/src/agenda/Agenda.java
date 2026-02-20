5ba1294ebd8403fa762c4ce720f9dcbb459d2b9a
unairuizd <ruizdavalosunai@gmail.com>
2026-02-20 13:25:50 +0100

    Crear clase contacto. Crear metodo agregarContacto.

--- AgendaColaborativa/src/agenda/Agenda.java
+++ AgendaColaborativa/src/agenda/Agenda.java
@@ -1,5 +1,12 @@
 package agenda;
 
 public class Agenda {
-
+	public void agregarContacto(Contacto c) {
+	    if (c == null) {
+	        System.out.println("El contacto no puede ser nulo.");
+	        return;
+	    }
+	    contactos.add(c);
+	    System.out.println("Contacto " + c.getNombre() + " agregado correctamente.");
+	}
 }
