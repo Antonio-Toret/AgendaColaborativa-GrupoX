41689c99fe7e736eee2e921a5b6cb889dcdca93c
atore <atore@Antonio-1>
2026-02-20 13:15:25 +0100

    Métodos buscarContacto y eliminarContacto añadidos en la clase agenda

--- AgendaColaborativa/src/agenda/Agenda.java
+++ AgendaColaborativa/src/agenda/Agenda.java
@@ -1,5 +1,21 @@
 package agenda;
 
 public class Agenda {
+	public Contacto buscarContacto(String nombre) {
+	    for (Contacto c : contactos) {
+	        if (c.getNombre().equalsIgnoreCase(nombre)) {
+	            return c;
+	        }
+	    }
+	    return null;
+	}
 
+	public boolean eliminarContacto(String nombre) {
+	    Contacto contacto = buscarContacto(nombre);
+	    if (contacto != null) {
+	        contactos.remove(contacto);
+	        return true;
+	    }
+	    return false;
+	}
 }
